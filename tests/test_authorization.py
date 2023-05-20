4d52c9c6c94a51cd4f21f208994a86b0c452aa19
KsyuSmile <nezy_ksuy@mail.ru>
2023-05-20 21:50:53 +0400

    Revert "Update conftest.py"
    This reverts commit 4563e120

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,7 +1,7 @@
 from config import valid_phone, valid_email, valid_login, valid_pass, valid_ls, invalid_phone, invalid_email
 
 
-def test_auth_by_phone_valid(browser, auth_elements, button_exit):
+def test_auth_by_phone_valid(auth_elements, button_exit):
     """Проверка авторизации по номеру телефона с валидными данными"""
 
     auth_elements.go_to_site()
@@ -11,7 +11,7 @@ def test_auth_by_phone_valid(browser, auth_elements, button_exit):
     assert button_exit.check_button_exit() == "Выйти"
 
 
-def test_auth_by_email_valid(browser, auth_elements, button_exit, auth_tab):
+def test_auth_by_email_valid(auth_elements, button_exit, auth_tab):
     """Проверяем авторизацию по адресу электронной почты с валидными данными"""
 
     auth_elements.go_to_site()
@@ -22,7 +22,7 @@ def test_auth_by_email_valid(browser, auth_elements, button_exit, auth_tab):
     assert button_exit.check_button_exit() == "Выйти"
 
 
-def test_auth_by_login_valid(browser, auth_elements, button_exit, auth_tab):
+def test_auth_by_login_valid(auth_elements, button_exit, auth_tab):
     """Проверяем авторизацию по логину с валидными данными"""
 
     auth_elements.go_to_site()
@@ -33,7 +33,7 @@ def test_auth_by_login_valid(browser, auth_elements, button_exit, auth_tab):
     assert button_exit.check_button_exit() == "Выйти"
 
 
-def test_auth_by_ls_valid(browser, auth_elements, button_exit, auth_tab):
+def test_auth_by_ls_valid(auth_elements, button_exit, auth_tab):
     """Проверяем авторизацию по лицевому счёту с валидными данными"""
 
     auth_elements.go_to_site()
@@ -44,7 +44,7 @@ def test_auth_by_ls_valid(browser, auth_elements, button_exit, auth_tab):
     assert button_exit.check_button_exit() == "Выйти"
 
 
-def test_auth_empty_value(browser, auth_elements, error_message):
+def test_auth_empty_value(auth_elements, error_message):
     """Проверяем авторизацию с пустыми полями для ввода"""
 
     auth_elements.go_to_site()
@@ -54,7 +54,7 @@ def test_auth_empty_value(browser, auth_elements, error_message):
     assert error_message.error_message_username() == "Введите номер телефона"
 
 
-def test_auth_by_phone_invalid(browser, auth_elements, error_message):
+def test_auth_by_phone_invalid(auth_elements, error_message):
     """Проверяем авторизацию по номеру телефона с неавторизованным номером телефона"""
 
     auth_elements.go_to_site()
@@ -64,7 +64,7 @@ def test_auth_by_phone_invalid(browser, auth_elements, error_message):
     assert error_message.error_message_login_end_pass() == "Неверный логин или пароль"
 
 
-def test_auth_by_email_invalid(browser, auth_elements, auth_tab, error_message):
+def test_auth_by_email_invalid(auth_elements, auth_tab, error_message):
     """Проверяем авторизацию по адресу электронной почты с неавторизованной почтой"""
 
     auth_elements.go_to_site()
@@ -75,7 +75,7 @@ def test_auth_by_email_invalid(browser, auth_elements, auth_tab, error_message):
     assert error_message.error_message_login_end_pass() == "Неверный логин или пароль"
 
 
-def test_click_button_register(browser, auth_elements, register_page):
+def test_click_button_register(auth_elements, register_page):
     """Проверяем кнопку 'Зарегистрироваться', переход на страницу регистрыции"""
 
     auth_elements.go_to_site()
@@ -83,7 +83,7 @@ def test_click_button_register(browser, auth_elements, register_page):
     assert register_page.check_section_title() == "Регистрация"
 
 
-def test_click_button_forgot_pass(browser, auth_elements, recovery_pass_page):
+def test_click_button_forgot_pass(auth_elements, recovery_pass_page):
     """Проверяем кнопку 'Забыл пароль', переход на страницу восстановления пароля"""
 
     auth_elements.go_to_site()
@@ -91,7 +91,7 @@ def test_click_button_forgot_pass(browser, auth_elements, recovery_pass_page):
     assert recovery_pass_page.check_section_title_pass() == "Восстановление пароля"
 
 
-def test_auth_email_auto_change_tab_from_phone(browser, auth_elements, auth_tab_active):
+def test_auth_email_auto_change_tab_from_phone(auth_elements, auth_tab_active):
     """Проверка автоматической смены таба выбора аутентификации при вводе электронной почты из вкладки Телефон"""
 
     auth_elements.go_to_site()
@@ -100,7 +100,7 @@ def test_auth_email_auto_change_tab_from_phone(browser, auth_elements, auth_tab_
     assert auth_tab_active.check_tab_email() == "Почта"
 
 
-def test_auth_login_auto_change_tab_from_phone(browser, auth_elements, auth_tab_active):
+def test_auth_login_auto_change_tab_from_phone(auth_elements, auth_tab_active):
     """Проверка автоматической смены таба выбора аутентификации при вводе логина из вкладки Телефон"""
 
     auth_elements.go_to_site()
@@ -109,7 +109,7 @@ def test_auth_login_auto_change_tab_from_phone(browser, auth_elements, auth_tab_
     assert auth_tab_active.check_tab_login() == "Логин"
 
 
-def test_auth_ls_auto_change_tab_from_phone(browser, auth_elements, auth_tab_active):
+def test_auth_ls_auto_change_tab_from_phone(auth_elements, auth_tab_active):
     """Проверка автоматической смены таба выбора аутентификации при вводе лицевого счёта из вкладки Телефон"""
 
     auth_elements.go_to_site()
@@ -118,7 +118,7 @@ def test_auth_ls_auto_change_tab_from_phone(browser, auth_elements, auth_tab_act
     assert auth_tab_active.check_tab_ls() == "Лицевой счёт"
 
 
-def test_auth_phone_auto_change_tab_from_email(browser, auth_elements, auth_tab, auth_tab_active):
+def test_auth_phone_auto_change_tab_from_email(auth_elements, auth_tab, auth_tab_active):
     """Проверка автоматической смены таба выбора аутентификации при вводе номера телефона из вкладки Почта"""
 
     auth_elements.go_to_site()
@@ -128,7 +128,7 @@ def test_auth_phone_auto_change_tab_from_email(browser, auth_elements, auth_tab,
     assert auth_tab_active.check_tab_phone() == "Телефон"
 
 
-def test_auth_login_auto_change_tab_from_email(browser, auth_elements, auth_tab, auth_tab_active):
+def test_auth_login_auto_change_tab_from_email(auth_elements, auth_tab, auth_tab_active):
     """Проверка автоматической смены таба выбора аутентификации при вводе логина из вкладки Почта"""
 
     auth_elements.go_to_site()
@@ -138,7 +138,7 @@ def test_auth_login_auto_change_tab_from_email(browser, auth_elements, auth_tab,
     assert auth_tab_active.check_tab_login() == "Логин"
 
 
-def test_auth_ls_auto_change_tab_from_email(browser, auth_elements, auth_tab, auth_tab_active):
+def test_auth_ls_auto_change_tab_from_email(auth_elements, auth_tab, auth_tab_active):
     """Проверка автоматической смены таба выбора аутентификации при вводе лицевого счёта из вкладки Почта"""
 
     auth_elements.go_to_site()
@@ -148,7 +148,7 @@ def test_auth_ls_auto_change_tab_from_email(browser, auth_elements, auth_tab, au
     assert auth_tab_active.check_tab_ls() == "Лицевой счёт"
 
 
-def test_auth_phone_auto_change_tab_from_login(browser, auth_elements, auth_tab, auth_tab_active):
+def test_auth_phone_auto_change_tab_from_login(auth_elements, auth_tab, auth_tab_active):
     """Проверка автоматической смены таба выбора аутентификации при вводе номера телефона из вкладки Логин"""
 
     auth_elements.go_to_site()
@@ -158,7 +158,7 @@ def test_auth_phone_auto_change_tab_from_login(browser, auth_elements, auth_tab,
     assert auth_tab_active.check_tab_phone() == "Телефон"
 
 
-def test_auth_email_auto_change_tab_from_login(browser, auth_elements, auth_tab, auth_tab_active):
+def test_auth_email_auto_change_tab_from_login(auth_elements, auth_tab, auth_tab_active):
     """Проверка автоматической смены таба выбора аутентификации при вводе электронной почты из вкладки Логин"""
 
     auth_elements.go_to_site()
@@ -168,7 +168,7 @@ def test_auth_email_auto_change_tab_from_login(browser, auth_elements, auth_tab,
     assert auth_tab_active.check_tab_email() == "Почта"
 
 
-def test_auth_ls_auto_change_tab_from_login(browser, auth_elements, auth_tab, auth_tab_active):
+def test_auth_ls_auto_change_tab_from_login(auth_elements, auth_tab, auth_tab_active):
     """Проверка автоматической смены таба выбора аутентификации при вводе лицевого счёта из вкладки Логин"""
 
     auth_elements.go_to_site()
@@ -178,7 +178,7 @@ def test_auth_ls_auto_change_tab_from_login(browser, auth_elements, auth_tab, au
     assert auth_tab_active.check_tab_ls() == "Лицевой счёт"
 
 
-def test_auth_phone_auto_change_tab_from_ls(browser, auth_elements, auth_tab, auth_tab_active):
+def test_auth_phone_auto_change_tab_from_ls(auth_elements, auth_tab, auth_tab_active):
     """Проверка автоматической смены таба выбора аутентификации при вводе номера телефона из вкладки Лицевой счёт"""
 
     auth_elements.go_to_site()
@@ -188,7 +188,7 @@ def test_auth_phone_auto_change_tab_from_ls(browser, auth_elements, auth_tab, au
     assert auth_tab_active.check_tab_phone() == "Телефон"
 
 
-def test_auth_email_auto_change_tab_from_ls(browser, auth_elements, auth_tab, auth_tab_active):
+def test_auth_email_auto_change_tab_from_ls(auth_elements, auth_tab, auth_tab_active):
     """Проверка автоматической смены таба выбора аутентификации при вводе электронной почты из вкладки Лицевой счёт"""
 
     auth_elements.go_to_site()
@@ -198,7 +198,7 @@ def test_auth_email_auto_change_tab_from_ls(browser, auth_elements, auth_tab, au
     assert auth_tab_active.check_tab_email() == "Почта"
 
 
-def test_auth_login_auto_change_tab_from_ls(browser, auth_elements, auth_tab, auth_tab_active):
+def test_auth_login_auto_change_tab_from_ls(auth_elements, auth_tab, auth_tab_active):
     """Проверка автоматической смены таба выбора аутентификации при вводе логина из вкладки Лицевой счёт"""
 
     auth_elements.go_to_site()
